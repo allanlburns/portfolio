65e80c05e4b75b2013177e70d81c2b75c6e20ca8
Allan Burns <allanlburns@yahoo.com>
2019-03-23 10:28:09 -0400

    Update About me blurb.

--- jobs/models.py
+++ jobs/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Job(models.Model):
 	image = models.ImageField(upload_to='images/')
 	summary = models.CharField(max_length=200)
+	url = models.URLField(max_length=200, default='#')
+	github_url = models.URLField(max_length=200, default='#')
+
 
 	def __str__(self):
 		return self.summary[:100]
